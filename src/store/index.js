21d121b6347e8504f676ac06da592d3605fb3c94
romluc <romulofilhofilho@gmail.com>
2019-10-16 16:41:00 -0300

    Change the state to object

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,28 @@
 import { createStore } from 'redux';
 
 function reducer() {
-	return [
-		{
-			id: 1,
-			title: 'Starting React',
-			lessons: [
-				{ id: 1, title: 'First class' },
-				{ id: 2, title: 'Second class' }
-			]
-		},
-		{
-			id: 2,
-			title: 'Learning Redux',
-			lessons: [
-				{ id: 3, title: 'Third class' },
-				{ id: 4, title: 'Fourth class' }
-			]
-		}
-	];
+	return {
+		activeLesson: null,
+		activeModule: null,
+		modules: [
+			{
+				id: 1,
+				title: 'Starting React',
+				lessons: [
+					{ id: 1, title: 'First class' },
+					{ id: 2, title: 'Second class' }
+				]
+			},
+			{
+				id: 2,
+				title: 'Learning Redux',
+				lessons: [
+					{ id: 3, title: 'Third class' },
+					{ id: 4, title: 'Fourth class' }
+				]
+			}
+		]
+	};
 }
 
 const store = createStore(reducer);
