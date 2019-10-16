e9cc532c57d081fa58aac172f36b9d00ffcf3fd2
romluc <romulofilhofilho@gmail.com>
2019-10-16 17:03:15 -0300

    Create action

--- src/store/index.js
+++ src/store/index.js
@@ -1,28 +1,37 @@
 import { createStore } from 'redux';
 
-function reducer() {
-	return {
-		activeLesson: null,
-		activeModule: null,
-		modules: [
-			{
-				id: 1,
-				title: 'Starting React',
-				lessons: [
-					{ id: 1, title: 'First class' },
-					{ id: 2, title: 'Second class' }
-				]
-			},
-			{
-				id: 2,
-				title: 'Learning Redux',
-				lessons: [
-					{ id: 3, title: 'Third class' },
-					{ id: 4, title: 'Fourth class' }
-				]
-			}
-		]
-	};
+const INITIAL_STATE = {
+	activeLesson: {},
+	activeModule: {},
+	modules: [
+		{
+			id: 1,
+			title: 'Starting React',
+			lessons: [
+				{ id: 1, title: 'First class' },
+				{ id: 2, title: 'Second class' }
+			]
+		},
+		{
+			id: 2,
+			title: 'Learning Redux',
+			lessons: [
+				{ id: 3, title: 'Third class' },
+				{ id: 4, title: 'Fourth class' }
+			]
+		}
+	]
+};
+
+function reducer(state = INITIAL_STATE, action) {
+	if (action.type === 'TOGGLE_LESSON') {
+		return {
+			...state,
+			activeLesson: action.lesson,
+			activeModule: action.module
+		};
+	}
+	return state;
 }
 
 const store = createStore(reducer);
